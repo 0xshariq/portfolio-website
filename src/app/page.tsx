5727a195aa35a3124020ad99698418438ab99d20
Shariq Choudhary <khanshariq92213@gmail.com>
2024-12-28 19:46:04 +0530

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,7 @@ export default function Page() {
               </div>
             </div>
           </BlurFade>
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] m-8">
             {DATA.projects.map((project, id) => (
               <div
                 key={project.title}
